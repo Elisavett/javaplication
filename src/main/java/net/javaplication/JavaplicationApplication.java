3e3abf338239b15030e84d8b49b3feccf95a5af5
evy11 <evy11@tpu.ru>
2020-11-24 12:31:32 +0700

    initial commint

---
+++ src/main/java/net/javaplication/JavaplicationApplication.java
@@ -0,0 +1,13 @@
+package net.javaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaplication/JavaplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
